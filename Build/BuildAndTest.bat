81685ebe16d31d70fc6a8e44b7889a720ef55708
Christoph Wille <christoph.wille@gmail.com>
2015-06-23 09:32:47 +0200

    Remove Author: from AssemblyInfo template

--- Build/BuildAndTest.bat
+++ Build/BuildAndTest.bat
@@ -1,6 +1,6 @@
 @echo This script simulates what the build server is doing
 @rem  /p:AdditionalBuildProperties="/v:d /p:MSBuildTargetsVerbose=true"
-%windir%\microsoft.net\framework\v4.0.30319\msbuild Automated.NRefactory.proj /p:ArtefactsOutputDir="%CD%\results" /p:TestReportsDir="%CD%\results"
+%windir%\microsoft.net\framework\v4.0.30319\msbuild Automated.RefactoringEssentials.proj /p:ArtefactsOutputDir="%CD%\results" /p:TestReportsDir="%CD%\results"
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @exit /B 0
 :err
