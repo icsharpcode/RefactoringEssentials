f99175230e94f73635144b8c5d8b9360675e6284
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-06-28 11:22:09 -0400

    Started porting this analyzer.Having some issues to figure out what write for the IsThrowsClause and IsRedundant methods.  Plus, I would just like to make sure that nothing is missing.

--- RefactoringEssentials/CSharp/Diagnostics/Synced/RedundanciesInCode/RedundantCatchClauseAnalyzer.cs
+++ RefactoringEssentials/CSharp/Diagnostics/Synced/RedundanciesInCode/RedundantCatchClauseAnalyzer.cs
@@ -1,11 +1,16 @@
+using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RefactoringEssentials.CSharp.CodeRefactorings;
 
 namespace RefactoringEssentials.CSharp.Diagnostics
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    [NotPortedYet]
     public class RedundantCatchClauseAnalyzer : DiagnosticAnalyzer
     {
         static readonly DiagnosticDescriptor descriptor = new DiagnosticDescriptor(
@@ -21,18 +26,21 @@ namespace RefactoringEssentials.CSharp.Diagnostics
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(descriptor);
 
-        // "Remove redundant catch clauses" / "Remove 'catch'" / "'try' statement is redundant" / "Remove all '{0}' redundant 'catch' clauses" / "Remove 'try' statement"
+        // "Remove redundant catch clauses" / "Remove 'catch'" / "'try' statement is redundant" /
+        //"Remove all '{0}' redundant 'catch' clauses" / "Remove 'try' statement"
         public override void Initialize(AnalysisContext context)
         {
-            //context.RegisterSyntaxNodeAction(
-            //	(nodeContext) => {
-            //		Diagnostic diagnostic;
-            //		if (TryGetDiagnostic (nodeContext, out diagnostic)) {
-            //			nodeContext.ReportDiagnostic(diagnostic);
-            //		}
-            //	}, 
-            //	new SyntaxKind[] { SyntaxKind.None }
-            //);
+            context.RegisterSyntaxNodeAction(
+                (nodeContext) =>
+                {
+                    Diagnostic diagnostic;
+                    if (TryGetDiagnostic(nodeContext, out diagnostic))
+                    {
+                        nodeContext.ReportDiagnostic(diagnostic);
+                    }
+                },
+                  SyntaxKind.CatchClause 
+            );
         }
 
         static bool TryGetDiagnostic(SyntaxNodeAnalysisContext nodeContext, out Diagnostic diagnostic)
@@ -40,125 +48,175 @@ namespace RefactoringEssentials.CSharp.Diagnostics
             diagnostic = default(Diagnostic);
             if (nodeContext.IsFromGeneratedCode())
                 return false;
-            //var node = nodeContext.Node as ;
-            //diagnostic = Diagnostic.Create (descriptor, node.GetLocation ());
-            //return true;
+
+            var catchClause = nodeContext.Node as CatchClauseSyntax ;
+            var tryStatement = catchClause?.Parent as TryStatementSyntax;
+            if (tryStatement == null || tryStatement.Finally != null)
+                return false;
+
+            var catchBlock = catchClause.ChildNodes().OfType<BlockSyntax>().First();
+
+            if (catchBlock != null || !catchBlock.ChildNodes().Any())
+            {
+                return false;
+            }
+
+            if (!IsThrowsClause(catchClause))
+                return false;
+
+            if (IsRedundant(catchClause, nodeContext))
+            {
+                diagnostic = Diagnostic.Create(descriptor, catchClause.GetLocation());
+                return true;
+            }
             return false;
         }
 
-        //		class GatherVisitor : GatherVisitorBase<RedundantCatchClauseAnalyzer>
-        //		{
-        //			public GatherVisitor(SemanticModel semanticModel, Action<Diagnostic> addDiagnostic, CancellationToken cancellationToken)
-        //				: base (semanticModel, addDiagnostic, cancellationToken)
-        //			{
-        //			}
+        static bool IsThrowsClause(CatchClauseSyntax catchClause)
+        {
+            var firstStatement = catchClause..Statements.FirstOrNullObject();
+            if (firstStatement == null)
+                return false;
+            var throwStatement = firstStatement as ThrowStatementSyntax;
 
-        ////			public override void VisitTryCatchStatement(TryCatchStatement tryCatchStatement)
-        ////			{
-        ////				var redundantCatchClauses = new List<CatchClause>();
-        ////				bool hasNonRedundantCatch = false;
-        ////				foreach (var catchClause in tryCatchStatement.CatchClauses) {
-        ////					if (IsRedundant(catchClause)) {
-        ////						redundantCatchClauses.Add(catchClause);
-        ////					} else {
-        ////						hasNonRedundantCatch = true;
-        ////					}
-        ////				}
-        ////
-        ////				if (hasNonRedundantCatch || !tryCatchStatement.FinallyBlock.IsNull) {
-        ////					AddDiagnosticAnalyzersForClauses(tryCatchStatement, redundantCatchClauses);
-        ////				} else {
-        ////					AddDiagnosticAnalyzerForTryCatchStatement(tryCatchStatement);
-        ////				}
-        ////			}
-        ////
-        ////			void AddDiagnosticAnalyzersForClauses(AstNode node, List<CatchClause> redundantCatchClauses)
-        ////			{
-        //			//				var allCatchClausesMessage = ctx.TranslateString("");
-        ////				var removeAllRedundantClausesAction = new CodeAction(allCatchClausesMessage, script => {
-        ////					foreach (var redundantCatchClause in redundantCatchClauses) {
-        ////						script.Remove(redundantCatchClause);
-        ////					}
-        ////				}, node);
-        ////				var singleCatchClauseMessage = ctx.TranslateString("");
-        ////                var redundantCatchClauseMessage = ctx.TranslateString("");
-        ////				foreach (var redundantCatchClause in redundantCatchClauses) {
-        ////					var closureLocalCatchClause = redundantCatchClause;
-        ////					var removeRedundantClauseAction = new CodeAction(singleCatchClauseMessage, script => {
-        ////						script.Remove(closureLocalCatchClause);
-        ////					}, node);
-        ////					var actions = new List<CodeAction>();
-        ////					actions.Add(removeRedundantClauseAction);
-        ////					if (redundantCatchClauses.Count > 1) {
-        ////						actions.Add(removeAllRedundantClausesAction);
-        ////					}
-        ////					AddDiagnosticAnalyzer(new CodeIssue(closureLocalCatchClause, redundantCatchClauseMessage, actions) { IssueMarker = IssueMarker.GrayOut });
-        ////				}
-        ////			}
-        ////
-        ////			void AddDiagnosticAnalyzerForTryCatchStatement(TryCatchStatement tryCatchStatement)
-        ////			{
-        ////				var lastCatch = tryCatchStatement.CatchClauses.LastOrNullObject();
-        ////				if (lastCatch.IsNull)
-        ////					return;
-        ////
-        ////				var removeTryCatchMessage = ctx.TranslateString("");
-        ////
-        ////				var removeTryStatementAction = new CodeAction(removeTryCatchMessage, script => {
-        ////					var statements = tryCatchStatement.TryBlock.Statements;
-        ////					if (statements.Count == 1 || tryCatchStatement.Parent is BlockStatement) {
-        ////						foreach (var statement in statements) {
-        ////							script.InsertAfter(tryCatchStatement.GetPrevSibling (s => !(s is NewLineNode)), statement.Clone());
-        ////						}
-        ////						script.Remove(tryCatchStatement);
-        ////					} else {
-        ////						var blockStatement = new BlockStatement();
-        ////						foreach (var statement in statements) {
-        ////							blockStatement.Statements.Add(statement.Clone());
-        ////						}
-        ////						script.Replace(tryCatchStatement, blockStatement);
-        ////					}
-        ////					// The replace and insert script functions does not format these things well on their own
-        ////					script.FormatText(tryCatchStatement.Parent);
-        ////				}, tryCatchStatement);
-        ////
-        ////				var fixes = new [] {
-        ////					removeTryStatementAction
-        ////				};
-        ////				AddDiagnosticAnalyzer(new CodeIssue(tryCatchStatement.TryBlock.EndLocation, lastCatch.EndLocation, removeTryCatchMessage, fixes) { IssueMarker = IssueMarker.GrayOut });
-        ////			}
-        ////
-        ////			static bool IsThrowsClause (CatchClause catchClause)
-        ////			{
-        ////				var firstStatement = catchClause.Body.Statements.FirstOrNullObject();
-        ////				if (firstStatement.IsNull)
-        ////					return false;
-        ////				var throwStatement = firstStatement as ThrowStatement;
-        ////				if (throwStatement == null || !throwStatement.Expression.IsNull)
-        ////					return false;
-        ////				return true;
-        ////			}
-        ////
-        ////			bool IsRedundant(CatchClause catchClause)
-        ////			{
-        ////				if (!IsThrowsClause (catchClause))
-        ////					return false;
-        ////
-        ////				var type = ctx.Resolve (catchClause.Type).Type;
-        ////				var n = catchClause.NextSibling;
-        ////				while (n != null) {
-        ////					var nextClause = n as CatchClause;
-        ////					if (nextClause != null) {
-        ////						if (nextClause.Type.IsNull && !IsThrowsClause(nextClause))
-        ////							return false;
-        ////						if (!IsThrowsClause(nextClause) && type.GetDefinition ().IsDerivedFrom (ctx.Resolve (nextClause.Type).Type.GetDefinition ()))
-        ////							return false;
-        ////					}
-        ////					n = n.NextSibling;
-        ////				}
-        ////				return true;
-        ////			}
-        //		}
+            return (throwStatement?.Expression != null);
+        }
+
+       static bool IsRedundant(CatchClauseSyntax catchClause, SyntaxNodeAnalysisContext nodeContext)
+        {
+            if (!IsThrowsClause(catchClause))
+                return false;
+            var type = nodeContext.SemanticModel.GetTypeInfo(catchClause).ConvertedType;
+            var n = catchClause
+            while (n != null)
+            {
+                var nextClause = n as CatchClause;
+                if (nextClause != null)
+                {
+                    if (nextClause.Type.IsNull && !IsThrowsClause(nextClause))
+                        return false;
+                    if (!IsThrowsClause(nextClause) && type.GetDefinition().IsDerivedFrom(ctx.Resolve(nextClause.Type).Type.GetDefinition()))
+                        return false;
+                }
+                n = n.NextSibling;
+            }
+            return true;
+        }
     }
+
+    //		class GatherVisitor : GatherVisitorBase<RedundantCatchClauseAnalyzer>
+    //		{
+    //			public GatherVisitor(SemanticModel semanticModel, Action<Diagnostic> addDiagnostic, CancellationToken cancellationToken)
+    //				: base (semanticModel, addDiagnostic, cancellationToken)
+    //			{
+    //			}
+
+    ////			public override void VisitTryCatchStatement(TryCatchStatement tryCatchStatement)
+    ////			{
+    ////				var redundantCatchClauses = new List<CatchClause>();
+    ////				bool hasNonRedundantCatch = false;
+    ////				foreach (var catchClause in tryCatchStatement.CatchClauses) {
+    ////					if (IsRedundant(catchClause)) {
+    ////						redundantCatchClauses.Add(catchClause);
+    ////					} else {
+    ////						hasNonRedundantCatch = true;
+    ////					}
+    ////				}
+    ////
+    ////				if (hasNonRedundantCatch || !tryCatchStatement.FinallyBlock.IsNull) {
+    ////					AddDiagnosticAnalyzersForClauses(tryCatchStatement, redundantCatchClauses);
+    ////				} else {
+    ////					AddDiagnosticAnalyzerForTryCatchStatement(tryCatchStatement);
+    ////				}
+    ////			}
+    ////
+    ////			void AddDiagnosticAnalyzersForClauses(AstNode node, List<CatchClause> redundantCatchClauses)
+    ////			{
+    //			//				var allCatchClausesMessage = ctx.TranslateString("");
+    ////				var removeAllRedundantClausesAction = new CodeAction(allCatchClausesMessage, script => {
+    ////					foreach (var redundantCatchClause in redundantCatchClauses) {
+    ////						script.Remove(redundantCatchClause);
+    ////					}
+    ////				}, node);
+    ////				var singleCatchClauseMessage = ctx.TranslateString("");
+    ////                var redundantCatchClauseMessage = ctx.TranslateString("");
+    ////				foreach (var redundantCatchClause in redundantCatchClauses) {
+    ////					var closureLocalCatchClause = redundantCatchClause;
+    ////					var removeRedundantClauseAction = new CodeAction(singleCatchClauseMessage, script => {
+    ////						script.Remove(closureLocalCatchClause);
+    ////					}, node);
+    ////					var actions = new List<CodeAction>();
+    ////					actions.Add(removeRedundantClauseAction);
+    ////					if (redundantCatchClauses.Count > 1) {
+    ////						actions.Add(removeAllRedundantClausesAction);
+    ////					}
+    ////					AddDiagnosticAnalyzer(new CodeIssue(closureLocalCatchClause, redundantCatchClauseMessage, actions) { IssueMarker = IssueMarker.GrayOut });
+    ////				}
+    ////			}
+    ////
+    ////			void AddDiagnosticAnalyzerForTryCatchStatement(TryCatchStatement tryCatchStatement)
+    ////			{
+    ////				var lastCatch = tryCatchStatement.CatchClauses.LastOrNullObject();
+    ////				if (lastCatch.IsNull)
+    ////					return;
+    ////
+    ////				var removeTryCatchMessage = ctx.TranslateString("");
+    ////
+    ////				var removeTryStatementAction = new CodeAction(removeTryCatchMessage, script => {
+    ////					var statements = tryCatchStatement.TryBlock.Statements;
+    ////					if (statements.Count == 1 || tryCatchStatement.Parent is BlockStatement) {
+    ////						foreach (var statement in statements) {
+    ////							script.InsertAfter(tryCatchStatement.GetPrevSibling (s => !(s is NewLineNode)), statement.Clone());
+    ////						}
+    ////						script.Remove(tryCatchStatement);
+    ////					} else {
+    ////						var blockStatement = new BlockStatement();
+    ////						foreach (var statement in statements) {
+    ////							blockStatement.Statements.Add(statement.Clone());
+    ////						}
+    ////						script.Replace(tryCatchStatement, blockStatement);
+    ////					}
+    ////					// The replace and insert script functions does not format these things well on their own
+    ////					script.FormatText(tryCatchStatement.Parent);
+    ////				}, tryCatchStatement);
+    ////
+    ////				var fixes = new [] {
+    ////					removeTryStatementAction
+    ////				};
+    ////				AddDiagnosticAnalyzer(new CodeIssue(tryCatchStatement.TryBlock.EndLocation, lastCatch.EndLocation, removeTryCatchMessage, fixes) { IssueMarker = IssueMarker.GrayOut });
+    ////			}
+    ////
+    ////			static bool IsThrowsClause (CatchClause catchClause)
+    ////			{
+    ////				var firstStatement = catchClause.Body.Statements.FirstOrNullObject();
+    ////				if (firstStatement.IsNull)
+    ////					return false;
+    ////				var throwStatement = firstStatement as ThrowStatement;
+    ////				if (throwStatement == null || !throwStatement.Expression.IsNull)
+    ////					return false;
+    ////				return true;
+    ////			}
+    ////
+    ////			bool IsRedundant(CatchClause catchClause)
+    ////			{
+    ////				if (!IsThrowsClause (catchClause))
+    ////					return false;
+    ////
+    ////				var type = ctx.Resolve (catchClause.Type).Type;
+    ////				var n = catchClause.NextSibling;
+    ////				while (n != null) {
+    ////					var nextClause = n as CatchClause;
+    ////					if (nextClause != null) {
+    ////						if (nextClause.Type.IsNull && !IsThrowsClause(nextClause))
+    ////							return false;
+    ////						if (!IsThrowsClause(nextClause) && type.GetDefinition ().IsDerivedFrom (ctx.Resolve (nextClause.Type).Type.GetDefinition ()))
+    ////							return false;
+    ////					}
+    ////					n = n.NextSibling;
+    ////				}
+    ////				return true;
+    ////			}
+    //		}
+}
 }
 
