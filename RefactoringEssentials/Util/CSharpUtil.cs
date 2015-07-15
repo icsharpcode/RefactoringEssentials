8c430bdb7614ed079ebd102aee40da8543e1871f
Andreas Weizel <rpinski@awzhome.de>
2015-07-16 01:42:27 +0200

    Added CreateChangedEventCodeRefactoring for VB.

--- RefactoringEssentials/Util/CSharpUtil.cs
+++ RefactoringEssentials/Util/CSharpUtil.cs
@@ -10,6 +10,7 @@ namespace RefactoringEssentials
     {
         public const string WorkspacesAsmName = ", Microsoft.CodeAnalysis.Workspaces, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
         public const string CSWorkspacesAsmName = ", Microsoft.CodeAnalysis.CSharp.Workspaces, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+        public const string VBWorkspacesAsmName = ", Microsoft.CodeAnalysis.VisualBasic.Workspaces, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
         public const string CAAsmName = ", Microsoft.CodeAnalysis, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
         public const string CACSharpAsmName = ", Microsoft.CodeAnalysis.CSharp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
     }
---
+++ RefactoringEssentials/Util/VBSyntaxExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+
+namespace RefactoringEssentials.Util
+{
+    static class VBSyntaxExtensions
+    {
+        public static IFieldSymbol ScanGetter(this AccessorBlockSyntax getter, SemanticModel model)
+        {
+            if (getter == null || getter.Statements.Count != 1)
+                return null;
+            var retStatement = getter.Statements.First() as ReturnStatementSyntax;
+            if (retStatement == null)
+                return null;
+            if (!IsPossibleExpression(retStatement.Expression))
+                return null;
+            var retSymbol = model.GetSymbolInfo(retStatement.Expression).Symbol;
+            return ((IFieldSymbol)retSymbol);
+        }
+
+        public static IFieldSymbol ScanSetter(this AccessorBlockSyntax setter, SemanticModel model)
+        {
+            if (setter == null || setter.Statements.Count != 1) //no getter/get;/get we can't easily work out
+                return null;
+            var setAssignment = setter.Statements.OfType<StatementSyntax>().FirstOrDefault();
+            var assignment = setAssignment != null ? setAssignment as AssignmentStatementSyntax : null;
+            if (assignment == null || !assignment.OperatorToken.IsKind(SyntaxKind.EqualsToken))
+                return null;
+            var id = assignment.Right as IdentifierNameSyntax;
+            if (id == null || id.Identifier.ValueText != "value")
+                return null;
+            if (!IsPossibleExpression(assignment.Left))
+                return null;
+            var retSymbol = model.GetSymbolInfo(assignment.Left).Symbol;
+            return ((IFieldSymbol)retSymbol);
+
+        }
+
+        static bool IsPossibleExpression(ExpressionSyntax left)
+        {
+            if (left.IsKind(SyntaxKind.IdentifierName))
+                return true;
+            var mr = left as MemberAccessExpressionSyntax;
+            if (mr == null)
+                return false;
+            return mr.Expression is MyClassExpressionSyntax;
+        }
+    }
+}
