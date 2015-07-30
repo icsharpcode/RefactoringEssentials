a92ad23408f019ef051c09e53bc4a1c410356aa9
Andreas Weizel <rpinski@awzhome.de>
2015-07-30 02:01:00 +0200

    Added some examples to VB demo project.

--- Demonstration.VB/MainModule.vb
+++ Demonstration.VB/MainModule.vb
@@ -2,6 +2,12 @@
 
     Sub Main()
 
+        Dim x As Integer = 0
+        ' Refactoring to convert "Do ... Loop until" to "Do Until ... Loop" and vice versa
+        Do
+            x += 1
+        Loop Until x = 5
+
     End Sub
 
 End Module
