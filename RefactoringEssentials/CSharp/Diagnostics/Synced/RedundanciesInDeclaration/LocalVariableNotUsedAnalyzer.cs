f7d513ab4313e336052776ca2e670ee4144d1678
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-08-10 13:03:29 -0400

    Geting nowhere with current analyzer. COming back later.

--- RefactoringEssentials/CSharp/Diagnostics/Synced/RedundanciesInDeclaration/LocalVariableNotUsedAnalyzer.cs
+++ RefactoringEssentials/CSharp/Diagnostics/Synced/RedundanciesInDeclaration/LocalVariableNotUsedAnalyzer.cs
@@ -28,14 +28,57 @@ namespace RefactoringEssentials.CSharp.Diagnostics
                 (nodeContext) =>
                 {
                     Diagnostic diagnostic;
-                    if (TryGetDiagnostic(nodeContext, out diagnostic))
+                    if (TryGetUnusedVariableDiagnostic(nodeContext, out diagnostic))
                     {
                         nodeContext.ReportDiagnostic(diagnostic);
                     }
                 },
-                new SyntaxKind[] { SyntaxKind.LocalDeclarationStatement }
+                SyntaxKind.LocalDeclarationStatement 
             );
         }
+
+        static bool TryGetUnusedVariableDiagnostic(SyntaxNodeAnalysisContext nodeContext, out Diagnostic diagnostic)
+        {
+            diagnostic = default(Diagnostic);
+            if (nodeContext.IsFromGeneratedCode())
+                return false;
+            var localDeclaration = nodeContext.Node as LocalDeclarationStatementSyntax;
+            if (localDeclaration == null)
+                return false;
+
+            if (localDeclaration.Declaration.Variables.Any() &&
+               localDeclaration.Declaration.Variables.First().Initializer != null)
+                return false;
+
+            if (localDeclaration.Declaration == null)
+                return false;
+            var initializer = localDeclaration.Declaration.Variables.FirstOrDefault().Initializer;
+            var declaredSymbol = nodeContext.SemanticModel.GetDeclaredSymbol(initializer) as ILocalSymbol;
+            if (declaredSymbol == null)
+                return false;
+
+            //			public override void VisitForEachStatement(ForEachStatementSyntax node)
+            //			{
+            //				base.VisitForEachStatement(node);
+
+            ////				var resolveResult = ctx.Resolve(foreachStatement.VariableNameToken) as LocalResolveResult;
+            ////				if (resolveResult == null)
+            ////					return;
+            ////
+            ////				if (IsUsed(foreachStatement, resolveResult.Variable, foreachStatement.VariableNameToken))
+            ////					return;
+            ////
+            ////				AddDiagnosticAnalyzer(new CodeIssue(foreachStatement.VariableNameToken, ctx.TranslateString("Local variable is never used")));
+            //			}
+
+            ////			bool IsUsed(SyntaxNode rootNode, ILocalSymbol variable, SyntaxNode variableNode)
+            ////			{
+            ////				return ctx.FindReferences(rootNode, variable).Any(result => result.Node != variableNode);
+            ////			}
+
+            return true;
+        }
+
 
         private static bool TryGetDiagnostic(SyntaxNodeAnalysisContext nodeContext, out Diagnostic diagnostic)
         {
@@ -44,6 +87,7 @@ namespace RefactoringEssentials.CSharp.Diagnostics
                 return false;
 
             var localDeclaration = nodeContext.Node as LocalDeclarationStatementSyntax;
+            var initializer = localDeclaration.Declaration.Variables;
             var variableInitializer = localDeclaration?.DescendantNodes().OfType<VariableDeclaratorSyntax>().First(); ;
 
             if (variableInitializer == null)
