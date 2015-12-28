a71a325728562273f24fc6363e8af2dbe64d1c0a
Christoph Wille <christoph.wille@gmail.com>
2015-12-28 20:45:45 +0100

    Add a minimal starter Web app for code conversion

---
+++ CodeConverterWebApp/Controllers/ConverterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CodeConverterWebApp.Models;
+
+namespace CodeConverterWebApp.Controllers
+{
+    public class ConverterController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]ConvertRequest todo)
+        {
+            string code = todo.code;
+
+            var response = new ConvertResponse()
+            {
+                conversionOk = false,
+                convertedCode = "",
+                errorMessage = "Not implemented"
+            };
+
+            return Ok(response);
+        }
+    }
+}
