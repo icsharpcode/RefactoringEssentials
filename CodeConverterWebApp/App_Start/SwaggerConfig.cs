d8abd21eb9889e391f8591711573dc08e4832582
Christoph Wille <christoph.wille@gmail.com>
2016-02-27 14:11:52 +0100

    Fix scheme and return type

--- CodeConverterWebApp/App_Start/SwaggerConfig.cs
+++ CodeConverterWebApp/App_Start/SwaggerConfig.cs
@@ -26,7 +26,7 @@ namespace CodeConverterWebApp
                         // the docs is taken as the default. If your API supports multiple schemes and you want to be explicit
                         // about them, you can use the "Schemes" option as shown below.
                         //
-                        //c.Schemes(new[] { "http", "https" });
+                        c.Schemes(new[] { "https" });
 
                         // Use "SingleApiVersion" to describe a single version API. Swagger 2.0 includes an "Info" object to
                         // hold additional metadata for an API. Version and title are required but you can also provide
