00516df694256dcfb4c57046b7fb09ef6d89dbe3
Siegfried Pammer <siegfriedpammer@gmail.com>
2016-09-30 20:26:26 +0200

    Add VB->C# option to web interface

--- RefactoringEssentials/Converter/ConversionResult.cs
+++ RefactoringEssentials/Converter/ConversionResult.cs
@@ -32,7 +32,7 @@ namespace RefactoringEssentials.Converter
             var builder = new StringBuilder();
             for (int i = 0; i < Exceptions.Count; i++)
             {
-                builder.AppendFormat("----- Exception {0} of {1} -----\r\n", i + 1, Exceptions.Count);
+                builder.AppendFormat("----- Exception {0} of {1} -----" + Environment.NewLine, i + 1, Exceptions.Count);
                 builder.AppendLine(Exceptions[i].ToString());
             }
             return builder.ToString();
