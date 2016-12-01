b5b9fe6821a9be80ec09c42b0bc8a8077d922f1c
Andreas Weizel <rpinski@awzhome.de>
2016-12-01 01:45:23 +0100

    Set correct Roslyn assembly versions for reflection (VS 2015 vs. VS 2017 variants).

---
+++ RefactoringEssentials/Util/ReflectionNamespaces.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefactoringEssentials
+{
+    class ReflectionNamespaces
+    {
+        public const string WorkspacesAsmName = ", Microsoft.CodeAnalysis.Workspaces, Version=1.2.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+        public const string CSWorkspacesAsmName = ", Microsoft.CodeAnalysis.CSharp.Workspaces, Version=1.2.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+        public const string VBWorkspacesAsmName = ", Microsoft.CodeAnalysis.VisualBasic.Workspaces, Version=1.2.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+        public const string CAAsmName = ", Microsoft.CodeAnalysis, Version=1.2.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+        public const string CACSharpAsmName = ", Microsoft.CodeAnalysis.CSharp, Version=1.2.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+    }
+}
