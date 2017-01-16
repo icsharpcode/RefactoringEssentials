f1b429faaf97f3bcf62f78f0ca179467f2ac0acc
Mike Krüger <mkrueger@xamarin.com>
2017-01-16 08:59:42 +0100

    Fixed 'Bug 51543 - Editor wrongly suggests a fix for extern static functions in DllImport'

--- RefactoringEssentials/CSharp/CodeRefactorings/Synced/ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider.cs
+++ RefactoringEssentials/CSharp/CodeRefactorings/Synced/ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider.cs
@@ -42,7 +42,8 @@ namespace RefactoringEssentials.CSharp.CodeRefactorings
             if (declaration == null
                 || declaration is BaseTypeDeclarationSyntax
                 || declaration is ConstructorDeclarationSyntax
-                || declaration is DestructorDeclarationSyntax)
+                || declaration is DestructorDeclarationSyntax
+                || declaration.GetBodies().IsEmpty())
                 return;
             var modifiers = declaration.GetModifiers();
             if (modifiers.Any(m => m.IsKind(SyntaxKind.OverrideKeyword)))
