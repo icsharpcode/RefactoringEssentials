icsharpcode/RefactoringEssentials

commit f1b429faaf97f3bcf62f78f0ca179467f2ac0acc

Fixed 'Bug 51543 - Editor wrongly suggests a fix for extern static functions in DllImport'

RefactoringEssentials/CSharp/CodeRefactorings/Synced/ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider.cs
@@ -42,7 +42,8 @@ namespace RefactoringEssentials.CSharp.CodeRefactorings
             if (declaration == null
                 || declaration is BaseTypeDeclarationSyntax
                 || declaration is ConstructorDeclarationSyntax
-                || declaration is DestructorDeclarationSyntax)
+                || declaration is DestructorDeclarationSyntax
+                || declaration.GetBodies().IsEmpty())
                 return;
             var modifiers = declaration.GetModifiers();
             if (modifiers.Any(m => m.IsKind(SyntaxKind.OverrideKeyword)))

Tests/CSharp/Diagnostics/FieldCanBeMadeReadOnlyTests.cs
@@ -338,6 +338,29 @@ class Test
 }
 ");
         }
+
+        [Test]
+        public void TestComments()
+        {
+            Analyze<FieldCanBeMadeReadOnlyAnalyzer>(@"class Test
+{
+    /// <summary>test</summary> 
+    object $fooBar$ = new object();
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(fooBar);
+    }
+}", @"class Test
+{
+    /// <summary>test</summary>
+    readonly object fooBar = new object();
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(fooBar);
+    }
+}");
+        }
+
     }
 }
 

commit 03a3eb07c701714ad9b442448a6056a28a68cbcc

fix bug introduced by f1b429faaf97f3bcf62f78f0ca179467f2ac0acc

RefactoringEssentials/CSharp/CodeRefactorings/Synced/ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider.cs
@@ -42,11 +42,10 @@ namespace RefactoringEssentials.CSharp.CodeRefactorings
             if (declaration == null
                 || declaration is BaseTypeDeclarationSyntax
                 || declaration is ConstructorDeclarationSyntax
-                || declaration is DestructorDeclarationSyntax
-                || declaration.GetBodies().IsEmpty())
+                || declaration is DestructorDeclarationSyntax)
                 return;
             var modifiers = declaration.GetModifiers();
-            if (modifiers.Any(m => m.IsKind(SyntaxKind.OverrideKeyword)))
+            if (modifiers.Any(m => m.IsKind(SyntaxKind.OverrideKeyword, SyntaxKind.ExternKeyword)))
                 return;
 
             TypeDeclarationSyntax enclosingTypeDeclaration = declaration.Ancestors().OfType<TypeDeclarationSyntax>().FirstOrDefault();
@@ -55,24 +54,8 @@ namespace RefactoringEssentials.CSharp.CodeRefactorings
 
             var explicitInterface = declaration.GetExplicitInterfaceSpecifierSyntax();
             if (explicitInterface != null)
-            {
                 return;
-            }
 
-            //			if (selectedNode != node.NameToken) {
-            //				if ((node is EventDeclaration && node is CustomEventDeclaration || selectedNode.Role != Roles.Identifier) && 
-            //					selectedNode.Role != IndexerDeclaration.ThisKeywordRole) {
-            //					var modToken = selectedNode as CSharpModifierToken;
-            //					if (modToken == null || (modToken.Modifier & (Modifiers.Abstract | Modifiers.Virtual)) == 0)
-            //						yield break;
-            //				} else {
-            //					if (!(node is EventDeclaration || node is CustomEventDeclaration) && selectedNode.Parent != node)
-            //						yield break;
-            //				}
-            //			}
-            //			if (!node.GetChildByRole(EntityDeclaration.PrivateImplementationTypeRole).IsNull)
-            //				yield break;
-            //
             if (enclosingTypeDeclaration.Modifiers.Any(m => m.IsKind(SyntaxKind.AbstractKeyword)))
             {
                 if (modifiers.Any(m => m.IsKind(SyntaxKind.AbstractKeyword)))

Tests/CSharp/CodeRefactorings/ToAbstractVirtualNonVirtualConversionTests.cs
@@ -101,7 +101,7 @@ class Test : IDisposable
 }");
         }
 
-		[Fact(Skip = "broken")]
+		[Fact]
 		public void AbstractToNonAbstractTest()
         {
             Test<ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider>(
@@ -117,7 +117,7 @@ class Test : IDisposable
 }");
         }
 
-		[Fact(Skip = "broken")]
+		[Fact]
 		public void AbstractToVirtualTest()
         {
             Test<ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider>(
@@ -133,7 +133,7 @@ class Test : IDisposable
 }", 1);
         }
 
-		[Fact(Skip = "broken")]
+		[Fact]
 		public void AbstractPropertyToNonAbstractTest()
         {
             Test<ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider>(
@@ -161,7 +161,7 @@ class Test : IDisposable
 }");
         }
 
-        [Fact(Skip="broken")]
+        [Fact]
         public void AbstractEventToNonAbstractTest()
         {
             Test<ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider>(
@@ -176,7 +176,7 @@ abstract class Test
 }");
         }
 
-		[Fact(Skip = "broken")]
+		[Fact]
 		public void NonAbstractToAbstractTest()
         {
             Test<ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider>(
@@ -192,7 +192,7 @@ abstract class Test
 }");
         }
 
-		[Fact(Skip = "broken")]
+		[Fact]
 		public void NonAbstractEventToAbstractTest()
         {
             Test<ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider>(
@@ -250,6 +250,20 @@ class Test
     public override string $ToString()
     {
     }
+}");
+        }
+
+        [Fact]
+        public void ExternMethodTest()
+        {
+            TestWrongContext<ToAbstractVirtualNonVirtualConversionCodeRefactoringProvider>(
+                @"using System;
+using System.Runtime.InteropServices;
+
+class Test
+{
+    [DllImport(""user32.dll"")]
+    static extern bool $CloseWindow(IntPtr hWnd);
 }");
         }
 