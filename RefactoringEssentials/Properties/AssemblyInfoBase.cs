472c2bfc28cbdf3d2c5d9802507f00dd4c341980
Siegfried Pammer <siegfriedpammer@gmail.com>
2017-02-19 16:38:35 +0100

    Update versioning info

--- RefactoringEssentials/Properties/AssemblyInfoBase.cs
+++ RefactoringEssentials/Properties/AssemblyInfoBase.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("ICSharpCode")]
 [assembly: AssemblyProduct("Refactoring Essentials")]
 [assembly: AssemblyCopyright("Copyright 2010-2016 AlphaSierraPapa and Xamarin Inc.")]
-[assembly: AssemblyVersion("4.5.0.0")]
-[assembly: AssemblyFileVersion("4.5.0")]
+[assembly: AssemblyVersion("4.9.0.0")]
+[assembly: AssemblyFileVersion("4.9.0")]
 
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
--- RefactoringEssentials/missing.md
+++ RefactoringEssentials/missing.md
@@ -5,7 +5,6 @@
 
 **C#**
 
-* ChangeAccessModifierAction
 * CreateIndexerAction
 * IntroduceConstantAction
 * LinqFluentToQueryAction
