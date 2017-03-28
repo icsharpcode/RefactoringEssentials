7aeb67a7abe95751bd4e9f24a8b72ecd47c783b9
Andreas Weizel <rpinski@awzhome.de>
2017-03-28 02:11:35 +0200

    Rebuilt generated files for VS 2015 solution versioning.

--- UpdateVersion.2015.ps1
+++ UpdateVersion.2015.ps1
@@ -6,30 +6,30 @@ $copyright = 'Copyright (c) 2014-2017 AlphaSierraPapa and Xamarin Inc.'
 # ========================================================================================
 
 # AppVeyor configuration
-(cat appveyor.yml) | % { $_ -replace 'version: (\d+\.\d+).\{build\}', ('version: ' + $version.Major + '.' + $version.Minor + '.{build}') } | Out-File appveyor.yml
+(cat appveyor.yml) | % { $_ -replace 'version: (\d+\.\d+).\{build\}', ('version: ' + $version.Major + '.' + $version.Minor + '.{build}') } | Out-File -Encoding "utf8" appveyor.yml
 
 # AssemblyInfoBase of RefactoringEssentials project
 (cat RefactoringEssentials/Properties/AssemblyInfoBase.cs) | % { $_ `
     -replace '\[assembly: AssemblyCopyright\(".+"\)\]', ('[assembly: AssemblyCopyright("' + $copyright + '")]') `
     -replace '\[assembly: AssemblyVersion\(".+"\)\]', ('[assembly: AssemblyVersion("' + $version.ToString() + '")]') `
     -replace '\[assembly: AssemblyFileVersion\(".+"\)\]', ('[assembly: AssemblyFileVersion("' + $version.ToString() + '")]') `
- } | Out-File RefactoringEssentials/Properties/AssemblyInfoBase.cs
+ } | Out-File -Encoding "utf8" RefactoringEssentials/Properties/AssemblyInfoBase.cs
 
  # NuGet defintions of RefactoringEssentials project
 (cat RefactoringEssentials/RefactoringEssentials.nuspec) | % { $_ `
     -replace '<copyright>.+</copyright>', ('<copyright>' + $copyright + '</copyright>') `
     -replace '<version>.+</version>', ('<version>{0}.{1}.{2}</version>' -f $version.Major, $version.Minor, $version.Build) `
     -replace 'https://github.com/icsharpcode/RefactoringEssentials/wiki/Release-.+', ('https://github.com/icsharpcode/RefactoringEssentials/wiki/Release-' + '{0}.{1}' -f $version.Major, $version.Minor) `
- } | Out-File RefactoringEssentials/RefactoringEssentials.nuspec
+ } | Out-File -Encoding "utf8" RefactoringEssentials/RefactoringEssentials.nuspec
  (cat RefactoringEssentials/RefactoringEssentials.Library.nuspec) | % { $_ `
     -replace '<copyright>.+</copyright>', ('<copyright>' + $copyright + '</copyright>') `
     -replace '<version>.+</version>', ('<version>{0}.{1}.{2}</version>' -f $version.Major, $version.Minor, $version.Build) `
     -replace 'https://github.com/icsharpcode/RefactoringEssentials/wiki/Release-.+', ('https://github.com/icsharpcode/RefactoringEssentials/wiki/Release-' + '{0}.{1}' -f $version.Major, $version.Minor) `
- } | Out-File RefactoringEssentials/RefactoringEssentials.Library.nuspec
+ } | Out-File -Encoding "utf8" RefactoringEssentials/RefactoringEssentials.Library.nuspec
 
 # VSIX manifest
 (cat Vsix/source.extension.vsixmanifest) | % { $_ `
     -replace 'Version="\d+\.\d+\.\d+\.\d+"', ('Version="' + $version.ToString() + '"') `
     -replace '<GettingStartedGuide>.+</GettingStartedGuide>', ('<GettingStartedGuide>' + $releaseNotesLink + '</GettingStartedGuide>') `
     -replace '<ReleaseNotes>.+</ReleaseNotes>', ('<ReleaseNotes>' + $releaseNotesLink + '</ReleaseNotes>') `
- } | Out-File Vsix/source.extension.vsixmanifest
+ } | Out-File -Encoding "utf8" Vsix/source.extension.vsixmanifest
