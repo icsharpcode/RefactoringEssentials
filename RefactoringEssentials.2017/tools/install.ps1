d18ab83f031cae0409dda002f3fbe87248468775
Andreas Weizel <rpinski@awzhome.de>
2017-10-14 13:55:36 +0200

    Had to remove the old files once again.

--- RefactoringEssentials.2017/tools/install.ps1
+++
@@ -1,6 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$analyzerPath = join-path (split-Path -path $toolsPath -parent) "analyzers\dotnet"
-$analyzerFilePath = join-path $analyzerPath "RefactoringEssentials.dll"
-
-$project.Object.AnalyzerReferences.Add("$analyzerFilePath")
