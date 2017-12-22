c2e069e0da370fe0c4259a3fd95ece164982c935
Andreas Weizel <rpinski@awzhome.de>
2017-12-22 11:49:33 +0100

    Updated version to 5.6.

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("ICSharpCode")]
 [assembly: AssemblyProduct("Refactoring Essentials for Visual Studio 2017")]
 [assembly: AssemblyCopyright("Copyright (c) 2014-2017 AlphaSierraPapa and Xamarin Inc.")]
-[assembly: AssemblyVersion("5.4.0.0")]
+[assembly: AssemblyVersion("5.6.0.0")]
 
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
